50d16e7c3d27745efd6924cd44c7c1de22ab7620
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-10-26 14:38:34 +0200

    dadi

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 */
 
-
+/*
 const listamail = [
   'sgiulia18@hotmail.it',
   'giuliasardelli@hotmail.it',
@@ -36,4 +36,44 @@ if (mailTrovata == true){
   console.log('mail non presente');
   document.getElementById('mail').innerHTML=
   `La mail non è nella lista`;
+}
+
++/
+
+// ****************************************************
+
+
+/*
+1-estrarre un numero random
+2-estrarre un numero random per il pc
+3-confrontarli
+4-stampare quello maggiore
+5-stampare il vincitore
+
+*/
+
+let mynum = (Math.floor(Math.random()*6)+1)
+
+console.log(mynum)
+
+document.getElementById('mydadi').innerHTML=
+`Il numero estratto per l'utente è: ${mynum}`;
+
+
+let pcnum = (Math.floor(Math.random()*6)+1)
+
+console.log(pcnum)
+
+document.getElementById('pcdadi').innerHTML=
+`Il numero estratto per il pc è: ${pcnum}`;
+
+if(mynum > pcnum){
+  document.getElementById('vincitore').innerHTML=
+  `Vince l'utente`;
+} else if(pcnum > mynum){
+  document.getElementById('vincitore').innerHTML=
+  `Vince il pc`;
+} else {
+  document.getElementById('vincitore').innerHTML=
+  `Vincono entrambi`;
 }
