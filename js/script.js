2b87f4fc8eff0c8622230f20dfb10542f6ac5918
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-10-26 13:55:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const listamail = [
+  'sgiulia18@hotmail.it',
+  'giuliasardelli@hotmail.it',
+  'giulia1234@gmail.com',
+  'sardelligiulia@libero.it',
+  'sardelligiulia1234@gmail.it'
+]
+
+console.log(listamail)
