58c42fff69507344d9a0c62eaa8f1bdb41eeada3
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-10-26 14:19:24 +0200

    email

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+/*
+1- creare una lista di mail
+2-
+
+*/
+
+
 const listamail = [
   'sgiulia18@hotmail.it',
   'giuliasardelli@hotmail.it',
@@ -5,5 +12,28 @@ const listamail = [
   'sardelligiulia@libero.it',
   'sardelligiulia1234@gmail.it'
 ]
+;
 
 console.log(listamail)
+
+let mailTrovata = false;
+
+const mymail = prompt('inserire mail');
+
+for (let i = 0; i < listamail.length; i++){
+
+  if(mymail === listamail[i]){
+    mailTrovata = true;
+    console.log('trovata')
+  }
+}
+
+if (mailTrovata == true){
+  console.log('mail presente');
+  document.getElementById('mail').innerHTML=
+  `La mail è nella lista`;
+} else{
+  console.log('mail non presente');
+  document.getElementById('mail').innerHTML=
+  `La mail non è nella lista`;
+}
